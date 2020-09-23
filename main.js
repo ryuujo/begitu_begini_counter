e50025dc2c51abf4f91d660b1274f02eba1dffc6
unknown <>
2020-09-23 10:30:57 +0700

    Kazoo

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 let beginiCount = 0;
 let begituCount = 0;
 let begonohCount = 0;
+let kazooCount = 0;
 
 this.addEventListener('keypress', (event) => {
   switch (event.key) {
+    case 'q':
+      begonohCount++;
+      break;
     case 'w':
       begituCount++;
       break;
     case 'e':
       beginiCount++;
       break;
-    case 'q':
-      begonohCount++;
+    case 'k':
+      kazooCount++;
       break;
     case 'p':
       beginiCount = 0;
       begituCount = 0;
       begonohCount = 0;
+      kazooCount = 0;
       break;
   }
   document.getElementById('begini').innerHTML = beginiCount;
   document.getElementById('begitu').innerHTML = begituCount;
   document.getElementById('begonoh').innerHTML = begonohCount;
-  console.log(beginiCount, begituCount, begonohCount);
+  document.getElementById('kazoo').innerHTML = kazooCount;
 });
