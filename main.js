5a71ef359c4ffe77af1dfdf5e6a8b49368161941
unknown <>
2020-09-23 10:39:14 +0700

    total counter

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 let beginiCount = 0;
+let beginiTotal = 0;
 let begituCount = 0;
+let begituTotal = 0;
 let begonohCount = 0;
+let begonohTotal = 0;
 let kazooCount = 0;
+let kazooTotal = 0;
 
 this.addEventListener('keypress', (event) => {
   switch (event.key) {
     case 'q':
       begonohCount++;
+      begonohTotal++;
       break;
     case 'w':
       begituCount++;
+      begituTotal++;
       break;
     case 'e':
       beginiCount++;
+      beginiTotal++;
       break;
     case 'k':
       kazooCount++;
+      kazooTotal++;
       break;
     case 'p':
       beginiCount = 0;
@@ -28,4 +36,8 @@ this.addEventListener('keypress', (event) => {
   document.getElementById('begitu').innerHTML = begituCount;
   document.getElementById('begonoh').innerHTML = begonohCount;
   document.getElementById('kazoo').innerHTML = kazooCount;
+  document.getElementById('beginitotal').innerHTML = beginiTotal;
+  document.getElementById('begitutotal').innerHTML = begituTotal;
+  document.getElementById('begonohtotal').innerHTML = begonohTotal;
+  document.getElementById('kazoototal').innerHTML = kazooTotal;
 });
